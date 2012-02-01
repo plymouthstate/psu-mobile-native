993957a2d4fe627a9288d1f429ea56e9c4597cef
Trevor Suarez <Rican7@gmail.com>
2012-02-01 17:51:20 -0500

    Revert "Removing the native folders to see if this will help PhoneGap Build recognize the config.xml file"
    This reverts commit 7cc3678901a9abd16ddae0433842bd8f5d19604f.

---
+++ PSU-Mobile-PhoneGap-Android/src/edu/plymouth/android/App.java
@@ -0,0 +1,19 @@
+package edu.plymouth.android;
+
+import android.os.Bundle;
+import com.phonegap.*;
+
+public class App extends DroidGap {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+        // Set a property to load the splash image before loading the URL
+        super.setIntegerProperty("splashscreen", R.drawable.splash);
+        
+        // Load the URL (page) that starts the app
+        // The second parameter is the amount of time that should pass before the URL is actually loaded... effectively the splash screen show time
+        super.loadUrl("https://www.dev.plymouth.edu/webapp/psu-mobile/", 2000);
+    }
+}
