b56dac1a55f428b0ff8f381f8ff21eb452c48625
Trevor Suarez <Rican7@gmail.com>
2012-06-13 13:10:48 -0400

    Upped the version number for new feature/plugin

--- www/js/psu-mobile.js
+++ www/js/psu-mobile.js
@@ -8,7 +8,7 @@
 	}
 
 	// Set the webapp URL as a variable
-	var appURL = 'https://www.dev.plymouth.edu/webapp/psu-mobile/?client-app=0.6.2';
+	var appURL = 'https://www.dev.plymouth.edu/webapp/psu-mobile/?client-app=0.8.0';
 
 	// Set the animation speed
 	var animationSpeed = 700; // milliseconds
